742069a92548d42a79dbb1b3b524f2b2a3be3c4c
msakg <msakg@DESKTOP-EAQ14N5>
2024-01-23 19:46:47 +0300

    Add project files.

---
+++ Application/Featrues/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Featrues.Brands.Commands.Create;
+using Application.Featrues.Brands.Commands.Delete;
+using Application.Featrues.Brands.Commands.Update;
+using Application.Featrues.Brands.Queries.GetById;
+using Application.Featrues.Brands.Queries.GetList;
+using AutoMapper;
+using Core.Application.Response;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Featrues.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            // CreateMap<Source, Destination>();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+            CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+            CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+            CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
+            CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+            CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+            CreateMap<Brand, DeleteBrandCommand>().ReverseMap();
+            CreateMap<Brand, DeletedBrandResponse>().ReverseMap();
+            CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
+        }
+    }
+}
---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,54 @@
+﻿using Application.Featrues.Brands.Commands.Create;
+using Application.Featrues.Brands.Commands.Delete;
+using Application.Featrues.Brands.Commands.Update;
+using Application.Featrues.Brands.Queries.GetById;
+using Application.Featrues.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand command)
+        {
+            var response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            GetByIdBrandQuery getByIdBrandQuery = new() { Id = id };
+            GetByIdBrandResponse response = await Mediator.Send(getByIdBrandQuery);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateBrandCommand command)
+        {
+            UpdatedBrandResponse response = await Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            DeletedBrandResponse response = await Mediator.Send(new DeleteBrandCommand { Id = id });
+            return Ok(response);
+        }
+    }
+}
